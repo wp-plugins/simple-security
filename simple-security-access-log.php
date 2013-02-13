c14cb4c9c88a57749b316a46bfc49c7eb74d622c
ChrisHurst <ChrisHurst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-13 22:33:38 +0000

    tagging v1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-security/tags/1.1.1@667660 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-security-access-log.php
@@ -0,0 +1,337 @@
+<?php
+
+//simple security access log
+
+class Simple_Security_Access_Log{
+
+
+	public $db_table;
+    
+    public $opt_name;
+	
+	
+	public $login_success = 0;
+	
+	public $data_labels = array();
+	
+	
+	
+	
+	public function __construct() {
+	
+		global $wpdb;
+		
+		//For translation purposes
+        $this->data_labels = array(
+            'Successful'        => __('Successful', 'simple_security'),
+            'Failed'            => __('Failed', 'simple_security'),
+            'Login'             => __('Login', 'simple_security'),
+            'User Agent'        => __('User Agent', 'simple_security'),
+            'Login Redirect'    => __('Login Redirect', 'simple_security'),
+            'id'                => __('#', 'simple_security'),
+            'uid'               => __('User ID', 'simple_security'),
+            'user_login'        => __('Username', 'simple_security'),
+            'user_role'         => __('User Role', 'simple_security'),
+            'time'              => __('Time', 'simple_security'),
+            'ip'                => __('IP Address', 'simple_security'),
+            'login_result'      => __('Login Result', 'simple_security'),
+            'data'              => __('Data', 'simple_security')
+        );
+
+	}
+	
+	
+	public function init(){
+	
+		add_action( 'admin_head', array($this, 'admin_header') );
+		add_action( 'admin_head', array($this, 'screen_options') );
+		add_action( 'admin_menu', array($this, 'simple_security_admin_menu') );
+		
+	}
+	
+	
+	function simple_security_admin_menu(){
+		global $simple_security_access_log_page;
+		
+       	$simple_security_access_log_page = add_submenu_page( 'users.php', __('Simple Access Log', 'simple_security'), __('Access Log', 'simple_security'), 'list_users', 'access_log', array(&$this, 'log_manager') );
+	   
+	   
+    }
+	
+	
+	
+	function log_manager(){
+
+        $log_table = $this->log_table;
+
+        $log_table->items = $this->log_get_data();
+        $log_table->prepare_items();
+
+
+		echo Simple_Security_Plugin::display_social_media(); 
+		
+		echo '<div class="wrap">';
+		
+			echo '<div id="icon-users" class="icon32"><br /></div>';
+			
+            echo '<h2>' . __('Simple Security Access Log', 'simple-security') . '</h2>';
+			
+			$this->show_tab_nav();
+			
+			//echo "<p><a href='".get_option('siteurl')."/wp-admin/options-general.php?page=simple-security-settings'>Simple Security Plugin Settings</a></p>";
+			
+            echo '<div class="tablenav top">';
+                echo '<div class="alignleft actions">';
+                    echo $this->date_filter();
+                echo '</div>';
+
+                $username = ( isset($_GET['filter']) ) ? esc_attr($_GET['filter']) : false;
+                echo '<form method="get" class="alignright">';
+                    echo '<p class="search-box">';
+                        echo '<input type="hidden" name="page" value="access_log" />';
+                        echo '<label>' . __('Username:', 'simple-security') . ' </label>';
+						echo '<input type="text" name="filter" class="filter-username" value="' . $username . '" />';
+						echo '<input class="button" type="submit" value="' . __('Filter User', 'simple-security') . '" />';
+                        echo '<br />';
+                    echo '</p>';
+                echo '</form>';
+            echo '</div>';
+			
+            echo '<div class="tablenav top">';
+				echo '<div class="alignleft actions">';
+						$log_table->views();
+				echo '</div>';
+
+                echo '<div class="alignright actions">';
+                $mode = ( isset($_GET['mode']) ) ? esc_attr($_GET['mode']) : false;
+                $log_table->view_switcher($mode);
+                echo '</div>';
+            echo '</div>';
+
+	        $log_table->display();
+			
+			//add classes to table rows based on success or failure
+			echo "<script>";
+			echo "jQuery(document).ready(function(){ ";
+				echo "jQuery('div.login-failed').parent().parent().addClass('login-failed'); ";
+				echo "jQuery('div.login-successful').parent().parent().addClass('login-successful'); ";
+			echo "})";
+			echo "</script>";
+			
+			
+			$ss_options = get_option($this->opt_name);
+			if($ss_options['basic_settings']['enable_ip_blacklist']){
+				//echo $this->ip_blacklist();
+			}
+			
+
+		echo '</div>';
+    }
+
+
+
+
+
+	private function show_tab_nav(){
+	
+		$tabs = array(
+			array('id' => 'basic_settings', 'title' => 'Basic Settings', 'link' => admin_url().'options-general.php?page=simple-security-settings&tab=basic_settings'),
+			array('id' => 'ip_blacklist', 'title' => 'IP Address Blacklist', 'link' => 'users.php?page=ip_blacklist'),			
+			array('id' => 'access_log', 'title' => 'Access Log', 'link' => 'users.php?page=access_log'),
+
+		);
+			
+	
+		echo '<h3 class="nav-tab-wrapper">';
+		
+		foreach( $tabs as $tab ){
+			$class = ( $tab['id'] == $_GET['page'] ) ? ' nav-tab-active' : '';
+			echo "<a class='nav-tab$class' href='".$tab['link']."'>".$tab['title']."</a>";
+		}
+		
+		echo '</h3>';
+		
+	}
+	
+	
+
+	function screen_options(){
+
+        //execute only on login_log page, othewise return null
+        $page = ( isset($_GET['page']) ) ? esc_attr($_GET['page']) : false;
+        if( 'access_log' != $page )
+            return;
+
+        $current_screen = get_current_screen();
+
+        //define options
+        $per_page_field = 'per_page';
+        $per_page_option = $current_screen->id . '_' . $per_page_field;
+
+        //Save options that were applied
+        if( isset($_REQUEST['wp_screen_options']) && isset($_REQUEST['wp_screen_options']['value']) ){
+            update_option( $per_page_option, esc_html($_REQUEST['wp_screen_options']['value']) );
+        }
+
+        //prepare options for display
+
+        //if per page option is not set, use default
+        $per_page_val = get_option($per_page_option, 20);
+        $args = array('label' => __('Records', 'simple-security'), 'default' => $per_page_val );
+
+        //display options
+        add_screen_option($per_page_field, $args);
+        $_per_page = get_option('users_page_access_log_per_page');
+
+        //create custom list table class to display log data
+        $this->log_table = new Simple_Security_Access_Log_List_Table;
+    }
+
+
+	function log_get_data(){
+        global $wpdb;
+
+        $where = '';
+
+        $where = $this->make_where_query();
+
+        if( is_array($where) && !empty($where) )
+            $where = 'WHERE ' . implode(' AND ', $where);
+
+        $sql = "SELECT * FROM $this->db_table $where ORDER BY time DESC";
+        $data = $wpdb->get_results($sql, 'ARRAY_A');
+
+        return $data;
+    }
+	
+	
+	/**
+	function ip_blacklist(){
+	
+	
+		$blacklist = array();		
+		if(isset($_POST['action']) && "add_blacklist_ip" == $_POST['action'] && is_admin()){
+			foreach($_POST['simple_security_ip_blacklist'] as $ip){
+				if($ip != ''){
+					$blacklist[] = $ip;
+				}
+			}
+			update_option('simple-security-ip-blacklist', $blacklist);
+		} 
+	
+		$i = 0;
+		$blacklist = get_option('simple-security-ip-blacklist');
+		
+		echo "<h2>Blocked IP Addresses</h2>";
+		echo "<a href='".get_option('siteurl')."/wp-admin/options-general.php?page=simple-security-settings'>IP Address Blacklist Settings</a>";
+		echo "<form method='post'>";
+		echo '<input type="hidden" name="page" value="access_log" />';
+		echo '<input type="hidden" name="action" value="add_blacklist_ip" />';
+		echo "<p>";
+		echo "Add New IP Address: <input type='text' name='simple_security_ip_blacklist[]' value=''>";
+		echo "<input type='submit' class='button'  value='Save'>";
+		echo "</p>";
+		echo "<hr>";
+		
+		if($blacklist = get_option('simple-security-ip-blacklist')){
+			foreach($blacklist as $ip){
+				echo "<p>Blocked IP Address: <input type='text' name='simple_security_ip_blacklist[ $i ]' value='$ip'></p>";
+				$i++;
+			}
+		}		
+		
+		echo "</form>";
+	
+	}
+	**/
+	
+	function make_where_query(){
+        $where = false;
+		
+        if( isset($_GET['filter']) && '' != $_GET['filter'] ){
+            $where['filter'] = "(user_login LIKE '%{$_GET['filter']}%' OR ip LIKE '%{$_GET['filter']}%')";
+        }
+		
+        if( isset($_GET['user_role']) && '' != $_GET['user_role'] ){
+            $where['user_role'] = "user_role = '{$_GET['user_role']}'";
+        }
+		
+        if( isset($_GET['result']) && '' != $_GET['result'] ){
+            $where['result'] = "login_result = '{$_GET['result']}'";
+        }
+		
+        if( isset($_GET['datefilter']) && '' != $_GET['datefilter'] ){
+            $year = substr($_GET['datefilter'], 0, 4);
+            $month = substr($_GET['datefilter'], -2);
+            $where['datefilter'] = "YEAR(time) = {$year} AND MONTH(time) = {$month}";
+        }
+
+        return $where;
+    }	
+	
+	
+
+	function date_filter(){
+        global $wpdb;
+        $sql = "SELECT DISTINCT YEAR(time) as year, MONTH(time)as month FROM {$this->db_table} ORDER BY YEAR(time), MONTH(time) desc";
+        $results = $wpdb->get_results($sql);
+
+        if(!$results)
+            return;
+
+
+        $option = '';
+        foreach($results as $row){
+            //represent month in double digits
+            $timestamp = mktime(0, 0, 0, $row->month, 1, $row->year);
+            $month = (strlen($row->month) == 1) ? '0' . $row->month : $row->month;
+            $datefilter = ( isset($_GET['datefilter']) ) ? $_GET['datefilter'] : false;
+            $option .= '<option value="' . $row->year . $month . '" ' . selected($row->year . $month, $datefilter, false) . '>' . date('F', $timestamp) . ' ' . $row->year . '</option>';
+        }
+
+        $output = '<form method="get">';
+        $output .= '<input type="hidden" name="page" value="access_log" />';
+        $output .= '<select name="datefilter"><option value="">' . __('View All', 'simple-security') . '</option>' . $option . '</select>';
+        $output .= '<input class="button" type="submit" value="' . __('Filter', 'simple-security') . '" />';
+        $output .= '</form>';
+        return $output;
+    }
+
+
+
+
+	function admin_header(){
+	
+        $page = ( isset($_GET['page']) ) ? esc_attr($_GET['page']) : false;
+        if( 'access_log' != $page )
+            return;
+
+        echo '<style type="text/css">';
+		
+        echo '.wp-list-table .column-id { width: 5%; }';
+        echo '.wp-list-table .column-uid { width: 10%; }';
+        echo '.wp-list-table .column-user_login { width: 10%; }';
+		echo '.wp-list-table .column-user_role { width: 10%; }';
+        echo '.wp-list-table .column-time { width: 15%; }';
+        echo '.wp-list-table .column-ip { width: 10%; }';
+        echo '.wp-list-table .column-login_result { width: 15%; }';
+		echo '.wp-list-table .column-data { width: 25%; }';
+		
+		echo "div.login-failed {color: red;}";
+		echo "div.login-successful {color: green;}";
+		
+		echo 'table.wp-list-table tbody tr.login-failed { background-color: rgba(255, 0, 0, 0.1) }';
+		echo 'table.wp-list-table tbody tr.login-failed.alternate { background-color: rgba(255, 0, 0, 0.05) }';
+		
+		echo 'table.wp-list-table tbody tr.login-successful { background-color: rgba(0, 255, 0, 0.1) }';
+		echo 'table.wp-list-table tbody tr.login-successful.alternate { background-color: rgba(0, 255, 0, 0.05) }';
+		
+		echo 'table.wp-list-table tbody tr:hover { background-color: rgba(255, 255, 0, 0.1) }';
+		echo 'table.wp-list-table tbody tr.alternate:hover { background-color: rgba(255, 255, 0, 0.1) }';
+		
+        echo '</style>';
+    }
+	
+}
+
+?>
---
+++ simple-security-ip-blacklist.php
@@ -0,0 +1,177 @@
+<?php
+
+
+class Simple_Security_IP_Blacklist{
+
+
+	public $opt_name;
+	
+
+	public function __construct() {
+	
+		global $wpdb;
+
+	}
+	
+	
+	public function init(){
+	
+		add_action( 'admin_menu', array($this, 'simple_security_admin_menu') );
+		
+	}
+	
+	
+	public function simple_security_admin_menu(){
+		
+	   	$ss_options = get_option($this->opt_name);
+		if($ss_options['basic_settings']['enable_ip_blacklist']){
+
+	  	 	global $simple_security_ip_blacklist;
+	  	 	$simple_security_ip_blacklist = add_submenu_page( 'users.php', __('IP Address Blacklist', 'simple_security'), __('IP Blacklist', 'simple_security'), 'list_users', 'ip_blacklist', array(&$this, 'ip_blacklist') );
+	   }
+	   
+    }
+
+
+
+
+	function ip_blacklist(){
+	
+	
+		$blacklist = array();		
+		if(isset($_POST['action']) && "add_blacklist_ip" == $_POST['action'] && is_admin()){
+			foreach($_POST['simple_security_ip_blacklist'] as $ip){
+				if($ip != ''){
+					$blacklist[] = $ip;
+				}
+			}
+			update_option('simple-security-ip-blacklist', $blacklist);
+		} 
+	
+		$i = 0;
+		$blacklist = get_option('simple-security-ip-blacklist');
+		
+		
+		echo "<style> 
+		.form-table{ clear:left; } 
+		.nav-tab-wrapper{ margin-bottom:0px; }
+		</style>";
+		
+		echo Simple_Security_Plugin::display_social_media(); 
+		
+		
+		echo '<div class="wrap">';
+		
+		
+			echo '<div id="icon-users" class="icon32"><br /></div>';
+			
+			echo "<h2>Blocked IP Addresses</h2>";
+			
+			$this->show_tab_nav();
+			
+			
+			echo "<form method='post'>";
+			echo '<div id="poststuff" class="metabox-holder has-right-sidebar">';
+			
+				echo '<div class="inner-sidebar">';
+					echo '<div id="side-sortables" class="meta-box-sortabless ui-sortable" style="position:relative;">';
+					
+						$sidebar = Simple_Security_Plugin::get_settings_sidebar();
+						Simple_Security_Settings_Page::set_sidebar($sidebar);
+						Simple_Security_Settings_Page::show_sidebar();
+					
+					echo '</div>';
+				echo '</div>';
+				
+				
+				
+				
+				echo '<div class="has-sidebar" >';			
+					echo '<div id="post-body-content" class="has-sidebar-content">';
+				
+			
+			
+						echo "<div id='blacklist_form' class='postbox'>\n";
+								
+							echo "<h3 class='hndle'><span>Add IP Address to Blacklist</span></h3>\n";
+								
+							echo "<div class='inside'>\n";
+						
+								echo '<input type="hidden" name="page" value="access_log" />';
+								echo '<input type="hidden" name="action" value="add_blacklist_ip" />';
+								echo "<p>";
+								echo "Add New IP Address: <input type='text' name='simple_security_ip_blacklist[]' value=''> ";
+								echo "</p>";
+								
+								echo '<div style="padding-left: 1.5em; margin-left:5px;">';
+								echo "<p class='submit'><input type='submit' class='button-primary'  value='Add IP Address To Blacklist'></p>";
+								echo "</div>";
+							
+							echo "</div>";
+						echo "</div>";
+						
+						
+						
+						echo "<div id='ip_blacklist' class='postbox'>\n";
+								
+							echo "<h3 class='hndle'><span>IP Address Blacklist</span></h3>\n";
+								
+							echo "<div class='inside'>\n";
+							
+								if($blacklist = get_option('simple-security-ip-blacklist')){
+									foreach($blacklist as $ip){
+										echo "<p>Blocked IP Address: <input type='text' name='simple_security_ip_blacklist[ $i ]' value='$ip'></p>";
+										$i++;
+									}
+								}		
+							
+								echo '<div style="padding-left: 1.5em; margin-left:5px;">';
+									echo "<p class='submit'><input type='submit' class='button-primary'  value='Update IP Address Blacklist'></p>";	
+								echo "</div>";
+								
+							echo "</div>";
+						echo "</div>";
+						
+						
+						
+					echo "</div>";	
+				echo "</div>";
+			echo "</div>";
+					
+					
+					
+				
+			echo "</form>";
+		
+		echo '</div>';
+	
+	
+	}
+
+
+
+	private function show_tab_nav(){
+	
+		$tabs = array(
+			array('id' => 'basic_settings', 'title' => 'Basic Settings', 'link' => admin_url().'options-general.php?page=simple-security-settings&tab=basic_settings'),
+			array('id' => 'ip_blacklist', 'title' => 'IP Address Blacklist', 'link' => 'users.php?page=ip_blacklist'),			
+			array('id' => 'access_log', 'title' => 'Access Log', 'link' => 'users.php?page=access_log'),
+		);
+			
+	
+		echo '<h3 class="nav-tab-wrapper">';
+		
+		foreach( $tabs as $tab ){
+			$class = ( $tab['id'] == $_GET['page'] ) ? ' nav-tab-active' : '';
+			echo "<a class='nav-tab$class' href='".$tab['link']."'>".$tab['title']."</a>";
+		}
+		
+		echo '</h3>';
+		
+	}
+	
+	
+
+}
+
+?>
